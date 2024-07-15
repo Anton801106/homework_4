50c24bab3cfefa0fe88dd2e8650a5f8e75a15b52
Anton801106 <listboxx@yandex.ru>
2024-07-15 18:49:38 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,10 @@
+# Организация программ и методы строк
+my_string = input('Здравствуйте, я выполняю 4-е задание: ')
+print(my_string)
+print(len(my_string))
+print(my_string)
+print(my_string .upper())
+print(my_string .lower())
+print(my_string .replace('Здравствуйте' , ' Привет'))
+print(my_string[0])
+print(my_string[-1])
